d402047accf88e8d86331121bc8be277af085d9b
Abdelrahman Elsayed <bodyomar79@gmail.com>
2024-05-18 05:38:31 +0300

    Update Client.java

--- client/src/main/java/com/example/Client.java
+++ client/src/main/java/com/example/Client.java
@@ -8,6 +8,8 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
 import org.apache.logging.log4j.LogManager;
 
+import static java.lang.Math.pow;
+
 public class Client extends Thread{
    private final int BATCH_SIZE = 10;
    private final int SLEEP_TIME = 1000;
@@ -30,40 +32,42 @@ public class Client extends Thread{
          Registry registry = LocateRegistry.getRegistry(configs.get("serverHost"), Integer.parseInt(configs.get("serverPort")));
          // lookup the graph object
          GraphService graphService = (GraphService) registry.lookup("graphService");
-         System.err.println("graphService found");
+//         System.err.println("graphService found");
 
          String graphName = graphService.getName();
-         System.out.println("Name:" + graphName);
+//         System.out.println("Name:" + graphName);
 
          int graphInitialSize = graphService.getInitialSize();
          logger.info("Graph Initial Size = " + graphInitialSize);
          logger.info("Graph Initial Name = " + graphName);
 
-         long totalResponseTime = 0;
-         
-         for(int i=0;i<5;++i){
 
+         for(int i=1;i<5;++i){
+            long totalResponseTime = 0;
             GraphBatchGenerator batch = new GraphBatchGenerator(graphInitialSize);
-            
+
             Random rand = new Random();
-            int updatePercentage = rand.nextInt(100);
-            // int updatePercentage = 67;
-            String requestBatch = batch.generateBatch(BATCH_SIZE,updatePercentage);
-            long startTime = System.currentTimeMillis();
-            String batchResult = graphService.processBatch(requestBatch,ALGORITHM_USED );
-            long endTime = System.currentTimeMillis();
+            int updatePercentage = 50;
+            int batch_size = (int) pow(10,i);
+            String requestBatch = batch.generateBatch(batch_size,updatePercentage);
+            String batchResult = null;
+            for(int j = 0; j < 5; j++){
+               long startTime = System.currentTimeMillis();
+               batchResult = graphService.processBatch(requestBatch,ALGORITHM_USED );
+               long endTime = System.currentTimeMillis();
+               long responseTime = endTime - startTime;
+               totalResponseTime += responseTime;
+            }
 
-            long responseTime = endTime - startTime;
-            totalResponseTime += responseTime;
+//            String log = batch.getLog(responseTime , batchResult ,requestBatch, ALGORITHM_USED , updatePercentage, BATCH_SIZE, Thread.currentThread().getId());
+//            String log = batch.getLog(totalResponseTime/5 , batchResult ,requestBatch, ALGORITHM_USED , updatePercentage, BATCH_SIZE, Thread.currentThread().getId());
+            logger.info(pow(10,i) + "size : "+totalResponseTime/5);
 
-            String log = batch.getLog(responseTime , batchResult ,requestBatch, ALGORITHM_USED , updatePercentage, BATCH_SIZE, Thread.currentThread().getId());
-            logger.info(log);
-            
             int sleepTime = rand.nextInt(SLEEP_TIME);
             // int sleepTime = SLEEP_TIME;
             Thread.sleep(sleepTime);
          }
-         logger.info("total Response time for Client " + totalResponseTime);
+//         logger.info("total Response time for Client " + totalResponseTime);
 
       } catch (Exception e) {
          logger.error(e);
